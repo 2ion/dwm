ffbdbf59a479652f58a94efaa4bfe77e4ae93912
Jens Oliver John <jens.o.john@gmail.com>
2012-10-14 13:44:20 +0200

    config.h: Enter Iceweasel rule for Iceweasel 16

--- config.h
+++ config.h
@@ -57,6 +57,7 @@ static const Rule rules[]           = {
 	/* class                    instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",                   NULL,       NULL,       0,            True,        -1 },
 	{ "Firefox",                NULL,       NULL,       1 << 1,       False,       -1 },
+    { "Iceweasel",              NULL,       NULL,       1 << 1,       False,       -1 },
     { "Luakit",                 NULL,       NULL,       1 << 1,       False,       -1 },
     { "MPlayer",                NULL,       NULL,       0,            True,        -1 },
     { "mplayer2",               NULL,       NULL,       0,            True,        -1 },
