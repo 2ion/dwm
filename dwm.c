dace157964e98b62bcb53d016c8a3dcc31572527
Jens John <jens.o.john@gmail.com>
2013-12-07 15:15:41 +0900

    added X11-apps/transset attribution

--- dwm.c
+++ dwm.c
@@ -20,11 +20,46 @@
  *
  * To understand everything else, start reading main().
  *
- * * 
+ * * *
  *
  * DWM
+ *
  * Patched and modified by Jens Oliver John <asterisk ! 2ion ! de>
  * Based on the DWM 6.0 release
+ *
+ * The code for setting/changing the X11 client opacity property was
+ * inspired by the X11 utility transset, which has the following license
+ * notice:
+ *
+ * <x11-apps: transset copyright notice>
+ *
+ * Copyright © 2003-2004 Matthew Hawn
+ * Copyright © 2003-2004 Andreas Kohn
+ * Copyright © 2003-2004 Roman Divacky
+ * Copyright © 2003-2004 Keith Packard
+ * Copyright © 2005-2007 Daniel Forchheimer
+ * Copyright © 2011-2012 Arnaud Fontaine
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * </x11-apps: transset copyright notice>
  *
  * Homepage: https://github.com/2ion/dwm
  */
