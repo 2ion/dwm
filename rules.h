1437883ee649ee26e0f3282bdbbe783fea348db2
Jens John <dev@2ion.de>
2015-03-16 01:10:55 +0100

    rules.h: add rule for gnome-weather

--- rules.h
+++ rules.h
@@ -1,19 +1,20 @@
 static const const Rule rules[] = {
-  /* class                      instance    title       tag           isfloating   monitor  opacity */
-    { "Gimp",                   NULL,       NULL,       TagLambda,    False,       -1,      1.0 },
-    { "Firefox",                NULL,       NULL,       TagWeb,       False,       -1,      1.0 },
-    { "Iceweasel",              NULL,       NULL,       TagWeb,       False,       -1,      1.0 },
-    { "Chromium",               NULL,       NULL,       TagWeb,       False,       -1,      1.0 },
-    { "MPlayer",                NULL,       NULL,       TagTerm,      True,        -1,      1.0 },
-    { "mplayer2",               NULL,       NULL,       TagTerm,      True,        -1,      1.0 },
-    { "mpv",                    NULL,       NULL,       TagTerm,      True,        -1,      1.0 },
-    { "Gjiten",                 NULL,       NULL,       TagDic,       False,       -1,      1.0 },
-    { "URxvt",                  NULL,       NULL,       TagTerm,      False,       -1,      1.0 },
-    { "Okular",                 NULL,       NULL,       TagPdf,       False,       -1,      1.0 },
-    { "MuPDF",                  NULL,       NULL,       TagPdf,       False,       -1,      1.0 },
-    { "EBView",                 NULL,       NULL,       TagDic,       False,       -1,      1.0 },
-    { "Filezilla",              NULL,       NULL,       TagKappa,     False,       -1,      1.0 },
-    { "Kiten",                  NULL,       NULL,       TagDic,       False,       -1,      1.0 }
+  /* class                            instance    title       tag           isfloating   monitor  opacity */
+    { "Gimp",                           NULL,       NULL,       TagLambda,    False,       -1,      1.0 },
+    { "Firefox",                        NULL,       NULL,       TagWeb,       False,       -1,      1.0 },
+    { "Iceweasel",                      NULL,       NULL,       TagWeb,       False,       -1,      1.0 },
+    { "Chromium",                       NULL,       NULL,       TagWeb,       False,       -1,      1.0 },
+    { "MPlayer",                        NULL,       NULL,       TagTerm,      True,        -1,      1.0 },
+    { "mplayer2",                       NULL,       NULL,       TagTerm,      True,        -1,      1.0 },
+    { "mpv",                            NULL,       NULL,       TagTerm,      True,        -1,      1.0 },
+    { "Gjiten",                         NULL,       NULL,       TagDic,       False,       -1,      1.0 },
+    { "URxvt",                          NULL,       NULL,       TagTerm,      False,       -1,      1.0 },
+    { "Okular",                         NULL,       NULL,       TagPdf,       False,       -1,      1.0 },
+    { "MuPDF",                          NULL,       NULL,       TagPdf,       False,       -1,      1.0 },
+    { "EBView",                         NULL,       NULL,       TagDic,       False,       -1,      1.0 },
+    { "Filezilla",                      NULL,       NULL,       TagKappa,     False,       -1,      1.0 },
+    { "Kiten",                          NULL,       NULL,       TagDic,       False,       -1,      1.0 },
+    { "Org.gnome.Weather.Application",  NULL,       NULL,       TagLambda,    True,        -1,      1.0 }
 };
 
 
